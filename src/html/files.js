5d61a3e31db87da9baa317d70a55a5a02f544b96
Aaron Perl <aaronjperl@gmail.com>
2016-09-26 18:50:47 -0400

    Added Doxyfile and generated doxygen documentation

---
+++ src/html/files.js
@@ -0,0 +1,7 @@
+var files =
+[
+    [ "HackBadge.cpp", "HackBadge_8cpp.html", null ],
+    [ "HackBadge.hpp", "HackBadge_8hpp.html", [
+      [ "HackBadge", "classHackBadge.html", "classHackBadge" ]
+    ] ]
+];
