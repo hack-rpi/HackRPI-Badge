995c48840af952095a1c694a7750ab16d57d8ef1
Aaron Perl <aaronjperl@gmail.com>
2016-09-26 19:03:09 -0400

    Regenerated doxygen documentation

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -1,6 +1,6 @@
 var NAVTREE =
 [
-  [ "HackBadgeLib", "index.html", [
+  [ "HackathonBadgeLibrary", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
